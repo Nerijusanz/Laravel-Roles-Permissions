96721bc648cf5f133bcdcef386e5ba37d7537682
Nerijus <nerijusanz@gmail.com>
2023-02-15 10:56:56 +0200

    crud (PermissionsController,RolesController)

--- app/Http/Controllers/Admin/PermissionsController.php
+++ app/Http/Controllers/Admin/PermissionsController.php
@@ -9,79 +9,52 @@ use App\Models\Permission;
 
 class PermissionsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function index()
     {
-        //
+        $permissions = Permission::all();
+
+        return view('admin.permissions.index', compact('permissions'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+        return view('admin.permissions.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StorePermissionRequest  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(StorePermissionRequest $request)
     {
-        //
+        $permission = Permission::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Permission  $permission
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Permission $permission)
     {
-        //
+        return view('admin.permissions.show', compact('permission'))
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Permission  $permission
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Permission $permission)
     {
-        //
+        return view('admin.permissions.edit', compact('permission'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdatePermissionRequest  $request
-     * @param  \App\Models\Permission  $permission
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(UpdatePermissionRequest $request, Permission $permission)
     {
-        //
+        $permission->update($request->all());
+
+        return redirect()->route('admin.permissions.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Permission  $permission
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Permission $permission)
     {
-        //
+        $permission->delete();
+
+        return back();
     }
 }
--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -5,83 +5,69 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreRoleRequest;
 use App\Http\Requests\UpdateRoleRequest;
+use App\Models\Permission;
 use App\Models\Role;
 
 class RolesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function index()
     {
-        //
+        $roles = Role::all();
+
+        return view('admin.roles.index', compact('roles'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+        $permissions = Permission::all()->pluck('title', 'id');
+
+        return view('admin.roles.create', compact('permissions'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreRoleRequest  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(StoreRoleRequest $request)
     {
-        //
+        $role = Role::create($request->all());
+        $role->permissions()->sync($request->input('permissions', []));
+
+        return redirect()->route('admin.roles.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Role  $role
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Role $role)
     {
-        //
+        $role->load('permissions');
+
+        return view('admin.roles.show', compact('role'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Role  $role
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Role $role)
     {
-        //
+        $permissions = Permission::all()->pluck('title', 'id');
+
+        $role->load('permissions');
+
+        return view('admin.roles.edit', compact('permissions', 'role'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateRoleRequest  $request
-     * @param  \App\Models\Role  $role
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(UpdateRoleRequest $request, Role $role)
     {
-        //
+        $role->update($request->all());
+        $role->permissions()->sync($request->input('permissions', []));
+
+        return redirect()->route('admin.roles.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Role  $role
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Role $role)
     {
-        //
+        $role->delete();
+
+        return back();
     }
 }
