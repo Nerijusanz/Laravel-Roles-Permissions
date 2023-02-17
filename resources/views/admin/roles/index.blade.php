a2d61f0976680d44ed98eb3dbc43789a6e5026b6
Nerijus <nerijusanz@gmail.com>
2023-02-17 16:08:17 +0200

    CREATED(roles crud)

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,81 +1,66 @@
 <x-admin-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ trans('cruds.role.title_singular') }} {{ trans('global.list') }}
+        {{ __('cruds.role.title_singular') }} {{ __('global.list') }}
         </h2>
     </x-slot>
             
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-
-                <a class="" href="{{ route('admin.roles.create') }}">
-                    {{ trans('global.add') }} {{ trans('cruds.role.title_singular') }}
-                </a>
-                
-
-                <table class="table-auto">
-                    <thead>
-                        <tr>
-                            <th width="10">
-
-                            </th>
-                            <th>
-                                {{ trans('cruds.role.fields.id') }}
-                            </th>
-                            <th>
-                                {{ trans('cruds.role.fields.title') }}
-                            </th>
-                            <th>
-                                &nbsp;
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($roles as $key => $role)
-                            <tr data-entry-id="{{ $role->id }}">
-                                <td>
-
-                                </td>
-                                <td>
-                                    {{ $role->id ?? '' }}
-                                </td>
-                                <td>
-                                    {{ $role->title ?? '' }}
-                                </td>
-                                <td>
-                                    
-                                        <a class="btn btn-xs btn-primary" href="{{ route('admin.roles.show', $role->id) }}">
-                                            {{ trans('global.view') }}
-                                        </a>
-                                    
-
-                                    
-                                        <a class="btn btn-xs btn-info" href="{{ route('admin.roles.edit', $role->id) }}">
-                                            {{ trans('global.edit') }}
-                                        </a>
-                                    
-
-                                    
-                                        <form action="{{ route('admin.roles.destroy', $role->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                            <input type="hidden" name="_method" value="DELETE">
-                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                            <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                        </form>
-                                    
-
-                                </td>
-
-                            </tr>
+    <a class="" href="{{ route('admin.roles.create') }}">
+        {{ __('global.add') }} {{ __('cruds.role.title_singular') }}
+    </a>
+    
+    <table class="">
+        <thead>
+            <tr>
+                <th>
+                    {{ __('cruds.role.fields.id') }}
+                </th>
+                <th>
+                    {{ __('cruds.role.fields.title') }}
+                </th>
+                <th>
+                    {{ __('cruds.role.fields.permissions') }}
+                </th>
+                <th>
+                    &nbsp;
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($roles as $key => $role)
+                <tr>
+                    <td>
+                        {{ $role->id ?? '' }}
+                    </td>
+                    <td>
+                        {{ $role->title ?? '' }}
+                    </td>
+                    <td>
+                        @foreach($role->permissions as $key => $item)
+                            <span class="">{{ $item->title }}</span>
                         @endforeach
-                    </tbody>
-                </table>
-                    
-                </div>
-            </div>
-
-
-        </div>
-    </div>
+                    </td>
+                    <td>
+                        
+                            <a class="" href="{{ route('admin.roles.show', $role->id) }}">
+                                {{ __('global.view') }}
+                            </a>
+                        
+                        
+                            <a class="" href="{{ route('admin.roles.edit', $role->id) }}">
+                                {{ __('global.edit') }}
+                            </a>
+                        
+                        
+                            <form action="{{ route('admin.roles.destroy', $role->id) }}" method="POST" onsubmit="return confirm('{{ __("global.areYouSure") }}');" style="display: inline-block;">
+                                <input type="hidden" name="_method" value="DELETE">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <input type="submit" class="" value="{{ __('global.delete') }}">
+                            </form>
+                        
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-admin-layout>
