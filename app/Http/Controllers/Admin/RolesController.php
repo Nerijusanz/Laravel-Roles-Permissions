b3b3a4046c57d9d2d71624991fffe6ff6fb5238c
Nerijus <nerijusanz@gmail.com>
2023-02-20 12:27:12 +0200

    ADD(Controller/Roles) roles controller crud auth gates

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\StoreRoleRequest;
 use App\Http\Requests\UpdateRoleRequest;
 use App\Models\Permission;
 use App\Models\Role;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class RolesController extends Controller
 {
@@ -14,6 +16,8 @@ class RolesController extends Controller
 
     public function index()
     {
+        abort_if(Gate::denies('role_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         $roles = Role::all();
 
         return view('admin.roles.index', compact('roles'));
@@ -22,6 +26,8 @@ class RolesController extends Controller
 
     public function create()
     {
+        abort_if(Gate::denies('role_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         $permissions = Permission::all()->pluck('title', 'id');
 
         return view('admin.roles.create', compact('permissions'));
@@ -30,8 +36,8 @@ class RolesController extends Controller
 
     public function store(StoreRoleRequest $request)
     {
-
         $role = Role::create($request->all());
+
         $role->permissions()->sync($request->input('permissions', []));
 
         return redirect()->route('admin.roles.index');
@@ -40,6 +46,8 @@ class RolesController extends Controller
 
     public function show(Role $role)
     {
+        abort_if(Gate::denies('role_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         $role->load('permissions');
 
         return view('admin.roles.show', compact('role'));
@@ -48,6 +56,8 @@ class RolesController extends Controller
 
     public function edit(Role $role)
     {
+        abort_if(Gate::denies('role_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         $permissions = Permission::all()->pluck('title', 'id');
 
         $role->load('permissions');
@@ -59,6 +69,7 @@ class RolesController extends Controller
     public function update(UpdateRoleRequest $request, Role $role)
     {
         $role->update($request->all());
+
         $role->permissions()->sync($request->input('permissions', []));
 
         return redirect()->route('admin.roles.index');
@@ -67,6 +78,8 @@ class RolesController extends Controller
 
     public function destroy(Role $role)
     {
+        abort_if(Gate::denies('role_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         $role->delete();
 
         return back();
--- app/Http/Requests/StoreRoleRequest.php
+++ app/Http/Requests/StoreRoleRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class StoreRoleRequest extends FormRequest
 {
@@ -13,6 +15,8 @@ class StoreRoleRequest extends FormRequest
      */
     public function authorize()
     {
+        abort_if(Gate::denies('role_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         return true;
     }
 
