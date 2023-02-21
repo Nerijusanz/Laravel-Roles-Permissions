6768d4fd24e690867a81bfa542d81604de4277e9
Nerijus <nerijusanz@gmail.com>
2023-02-21 11:29:49 +0200

    ADD(PermissionsApiController) crud

--- app/Http/Controllers/Api/V1/Admin/PermissionsApiController.php
+++ app/Http/Controllers/Api/V1/Admin/PermissionsApiController.php
@@ -3,62 +3,60 @@
 namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePermissionRequest;
+use App\Http\Requests\UpdatePermissionRequest;
+use App\Models\Permission;
+use App\Http\Resources\Admin\PermissionResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class PermissionsApiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        abort_if(Gate::denies('permission_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return new PermissionResource(Permission::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+
+    public function store(StorePermissionRequest $request)
     {
-        //
+        $permission = Permission::create($request->all());
+
+        return (new PermissionResource($permission))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+
+    public function show(Permission $permission)
     {
-        //
+        abort_if(Gate::denies('permission_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return new PermissionResource($permission);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+
+    public function update(UpdatePermissionRequest $request, Permission $permission)
     {
-        //
+        $permission->update($request->all());
+
+        return (new PermissionResource($permission))
+            ->response()
+            ->setStatusCode(Response::HTTP_ACCEPTED);
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+
+    public function destroy(Permission $permission)
     {
-        //
+        abort_if(Gate::denies('permission_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $permission->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
