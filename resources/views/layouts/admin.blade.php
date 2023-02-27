ba072cb84aa592120954008b80c6da23b231cfc5
Nerijus <nerijusanz@gmail.com>
2023-02-27 15:34:33 +0200

    CREATE(layouts/admin) coreui starter template

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,32 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ trans('panel.site_title') }}</title>
 
-        <title>{{ trans('panel.site_title') }}</title>
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.0/dist/css/coreui.min.css" rel="stylesheet" integrity="sha384-UkVD+zxJKGsZP3s/JuRzapi4dQrDDuEf/kHphzg8P3v8wuQ6m9RLjTkPGeFcglQU" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
         @yield('styles')
-    </head>
-
-    <body class="antialiased">
-
-        @include('layouts.navigation')
-
-        @if (isset($header))
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif    
-
-        <div class="mx-auto min-h-screen flex flex-col bg-gray-100">
-            {{ $slot }}
-        </div>
-        @yield('scripts')
+  </head>
+  <body>
+    
+    {{ $slot }}
+    
+    <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.0/dist/js/coreui.bundle.min.js" integrity="sha384-n0qOYeB4ohUPebL1M9qb/hfYkTp4lvnZM6U6phkRofqsMzK29IdkBJPegsyfj/r4" crossorigin="anonymous"></script>
+    @yield('scripts')
     </body>
 </html>
