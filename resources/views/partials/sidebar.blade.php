8ce7053d0c7955491f102f30770cfc95df1d083d
Nerijus <nerijusanz@gmail.com>
2023-02-27 16:26:53 +0200

    ADD(sidebar) user_management_access navigaion links

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,12 +1,58 @@
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
 
     <div class="sidebar-brand d-none d-md-flex">
-    {{ trans('panel.site_title') }}
+    {{ __('panel.site_title') }}
     </div>
 
     <ul class="sidebar-nav" data-coreui="navigation">
-        <li class="nav-item"><a class="nav-link" href="/">
-            Home
+
+        <li class="nav-item"><a class="nav-link" href="index.html">
+            <a href="{{ route('admin.home') }}" class="nav-link">
+                {{ __('global.dashboard') }}
+            </a>
         </li>
+
+        @can('user_management_access')
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link  nav-dropdown-toggle" href="#">
+                    <i class="fa-fw fas fa-users nav-icon">
+
+                    </i>
+                    {{ __('cruds.userManagement.title') }}
+                </a>
+                <ul class="nav-dropdown-items">
+                    @can('permission_access')
+                        <li class="nav-item">
+                            <a href="{{ route('admin.permissions.index') }}" class="nav-link {{ request()->is('admin/permissions') || request()->is('admin/permissions/*') ? 'active' : '' }}">
+                                <i class="fa-fw fas fa-unlock-alt nav-icon">
+
+                                </i>
+                                {{ __('cruds.permission.title') }}
+                            </a>
+                        </li>
+                    @endcan
+                    @can('role_access')
+                        <li class="nav-item">
+                            <a href="{{ route('admin.roles.index') }}" class="nav-link {{ request()->is('admin/roles') || request()->is('admin/roles/*') ? 'active' : '' }}">
+                                <i class="fa-fw fas fa-briefcase nav-icon">
+
+                                </i>
+                                {{ __('cruds.role.title') }}
+                            </a>
+                        </li>
+                    @endcan
+                    @can('user_access')
+                        <li class="nav-item">
+                            <a href="{{ route('admin.users.index') }}" class="nav-link {{ request()->is('admin/users') || request()->is('admin/users/*') ? 'active' : '' }}">
+                                <i class="fa-fw fas fa-user nav-icon">
+
+                                </i>
+                                {{ __('cruds.user.title') }}
+                            </a>
+                        </li>
+                    @endcan
+                </ul>
+            </li>
+        @endcan
     </ul>
 </div>
