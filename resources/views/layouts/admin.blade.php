44c623a73794fe8192fdb295f58ed757c8d0460b
Nerijus <nerijusanz@gmail.com>
2023-02-28 11:38:18 +0200

    ADD(layout.admin) coreui content

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,16 +4,34 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ trans('panel.site_title') }}</title>
+    <title>{{ __('panel.site_title') }}</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.0/dist/css/coreui.min.css" rel="stylesheet" integrity="sha384-UkVD+zxJKGsZP3s/JuRzapi4dQrDDuEf/kHphzg8P3v8wuQ6m9RLjTkPGeFcglQU" crossorigin="anonymous">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
         @yield('styles')
   </head>
   <body>
+
     @include('partials.sidebar')
-    {{ $slot }}
-    
+
+    <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+      <header class="header header-sticky mb-4">
+        <div class="container-lg">
+          
+        </div>
+        
+      </header>
+      <div class="body flex-grow-1 px-3">
+        <div class="container-lg">
+          {{$slot}}
+        </div>
+      </div>
+      <footer class="footer">
+        <div class="container-lg">
+          Footer
+        </div>
+      </footer>
+    </div>
     <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
         {{ csrf_field() }}
     </form>
